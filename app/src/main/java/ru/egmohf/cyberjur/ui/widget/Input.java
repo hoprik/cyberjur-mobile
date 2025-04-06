bbd4a507491a2173f6d0795825cc98473ad81125
Hoprik V. Morozov <hoprik@hoprik.ru>
2025-04-06 15:57:12 +0300

    пофиксил и Input

--- app/src/main/java/ru/egmohf/cyberjur/ui/widget/Input.java
+++ app/src/main/java/ru/egmohf/cyberjur/ui/widget/Input.java
@@ -1,12 +1,89 @@
 package ru.egmohf.cyberjur.ui.widget;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import androidx.appcompat.widget.AppCompatEditText;
+import ru.egmohf.cyberjur.R;
+import ru.egmohf.cyberjur.saveData.Cache;
+import ru.egmohf.cyberjur.ui.widget.helpers.BaseDrawable;
+import ru.egmohf.cyberjur.ui.widget.helpers.DrawableHelper;
+import ru.egmohf.cyberjur.ui.widget.helpers.IBaseDrawable;
 
-public class Input extends AppCompatEditText {
+public class Input extends AppCompatEditText implements IBaseDrawable {
+    private DrawableHelper helper;
     public Input(Context context, AttributeSet attrs) {
         super(context, attrs);
-        new BaseDrawable(this, attrs);
+
+        if (attrs != null){
+            TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.BaseWidget);
+            setAttributes(attributes);
+        }
+
+        BaseDrawable drawable = new BaseDrawable(this, attrs);
+        this.helper = new DrawableHelper(drawable);
+    }
+
+    public Input(Context context){
+        super(context);
+        BaseDrawable drawable = new BaseDrawable(this, null);
+        this.helper = new DrawableHelper(drawable);
+    }
+
+    private void setAttributes(TypedArray a) {
+        String textColor = a.getString(R.styleable.BaseWidget_txtColor);
+        setTextColor(textColor);
+        String textHintColor = a.getString(R.styleable.BaseWidget_txtHintColor);
+        setHintTextColor(textHintColor);
+        a.recycle();
+    }
+
+    public void setTextColor(String textColor) {
+        if (textColor == null) textColor = "#ffffff";
+        if (Cache.getInstance() != null && Cache.getInstance().hasData(textColor))
+            textColor = Cache.getInstance().getStringData(textColor, "");
+        setTextColor(Color.parseColor(textColor));
+    }
+
+    public void setHintTextColor(String textHintColor) {
+        if (textHintColor == null) textHintColor = "#80ffffff";
+        if (Cache.getInstance() != null && Cache.getInstance().hasData(textHintColor))
+            textHintColor = Cache.getInstance().getStringData(textHintColor, "");
+        setHintTextColor(Color.parseColor(textHintColor));
+    }
+
+    public void setBgColor(String color) {
+        this.helper.setBgColor(color);
+    }
+
+    @Override
+    public String getBgColor() {
+        return this.helper.getBgColor();
+    }
+
+    @Override
+    public void setRound(int round) {
+        this.helper.setRound(round);
+    }
+
+    @Override
+    public void setRoundX(int round) {
+        this.helper.setRoundX(round);
+    }
+
+    @Override
+    public void setRoundY(int round) {
+        this.helper.setRoundY(round);
+    }
+
+    @Override
+    public void setRound(int topLeft, int topRight, int bottomRight, int bottomLeft) {
+        this.helper.setRound(topLeft, topRight, bottomRight, bottomLeft);
+    }
+
+    @Override
+    public float[] getRound() {
+        return helper.getRound();
     }
 }
